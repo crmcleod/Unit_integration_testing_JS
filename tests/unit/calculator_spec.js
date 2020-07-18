73fa13648c4c50cf4b87b650b58376f5056866c9
Craig McLeod <mcleod.craig@outlook.com>
2020-07-18 13:44:16 +0100

    Refactored tests slightly, multiply and divide passing tests

--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -14,13 +14,29 @@ describe('calculator', function () {
     calculator.numberClick(1)
     calculator.operatorClick()
     calculator.add(4)
+    calculator.operatorClick()
     assert.strictEqual(calculator.runningTotal, 5)
   })
   it('it should subtract 2 numbers correctly', function(){
     calculator.numberClick(7)
     calculator.operatorClick()
     calculator.subtract(4)
+    calculator.operatorClick()
     assert.strictEqual(calculator.runningTotal, 3)
+  })
+  it('it should be multiply two numbers together correctly', function(){
+    calculator.numberClick(3)
+    calculator.operatorClick()
+    calculator.multiply(5)
+    calculator.operatorClick()
+    assert.strictEqual(calculator.runningTotal, 15)
+  })
+  it('should be able to divide one number by another correctly', function(){
+    calculator.numberClick(21)
+    calculator.operatorClick()
+    calculator.divide(3)
+    calculator.operatorClick()
+    assert.strictEqual(calculator.runningTotal, 7)
   })
 
 });
